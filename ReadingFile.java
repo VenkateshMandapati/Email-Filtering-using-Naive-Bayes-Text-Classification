2a56937277e1e41ff45a2270e889342b51e6fd8e
Venkatesh Mandapati <31746024+VenkateshMandapati@users.noreply.github.com>
2017-10-02 19:20:01 -0500

    Update ReadingFile.java

--- ReadingFile.java
+++ ReadingFile.java
@@ -11,11 +11,8 @@ import java.io.IOException;
 public class ReadingFile {
 
 	public static void main(String[] args) throws IOException {
-		// TODO Auto-generated method stub
-		//String address = "K:/Master's/4thSEM/DC/connectivity1.txt";
 		String address = args[0];
 		File file = new File(address);
-		//File tempFile = new File("K:/Master's/4thSEM/DC/connectivity2.txt");
 		File tempFile = new File(args[1]);
 		BufferedReader reader = new BufferedReader(new FileReader(file));
 		BufferedWriter writer = new BufferedWriter(new FileWriter(tempFile));
